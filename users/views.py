d56eb35ee5b20a4d2ddb5c127e7462f22292ea43
Adm <anton.chubshev@yandex.ru>
2025-07-02 15:19:02 +0300

    added send_welcome_email

--- users/views.py
+++ users/views.py
@@ -1,3 +1,4 @@
+from django.core.mail import send_mail
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
 
@@ -9,3 +10,15 @@ class UserCreateView(CreateView):
     model = User
     form_class = UserRegisterForm
     success_url = reverse_lazy('users:login')
+
+    def form_valid(self, form):
+        user = form.save()
+        self.send_welcome_email(user.email)
+        return super().form_valid(form)
+
+    def send_welcome_email(self, user_email):
+        subject = 'Добро пожаловать в наш сервис!'
+        message = 'Спасибо за регистрацию в моем web-приложении!'
+        from_email = 'chubshev.a@shtoff.ru'
+        recipient_list = [user_email,]
+        send_mail(subject, message, from_email, recipient_list)
